0a0cb5137f24ac08a9d1bcf59f73d5726d40055e
achau6 <52433480+achau6@users.noreply.github.com>
2021-05-23 19:25:43 -0700

    Update achau020_lab10_part3.c

--- turnin/achau020_lab10_part3.c
+++ turnin/achau020_lab10_part3.c
@@ -6,6 +6,7 @@
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ *	Demo: https://youtu.be/t6s0hElcYFo
  */
 #include <avr/io.h>
 #include "timer.h"
